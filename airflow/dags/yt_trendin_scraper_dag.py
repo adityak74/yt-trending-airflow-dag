6d377d77e8ee4cfea3eb06d5be9added6658734e
Aditya Karnam <akarnam37@gmail.com>
2022-03-14 00:36:59 -0500

    chore: refactor

--- airflow/dags/yt_trendin_scraper_dag.py
+++ airflow/dags/yt_trendin_scraper_dag.py
@@ -3,14 +3,14 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 from src.scrape_yt_explore import get_youtube_trending_api
 
-def helloWorld():
+def scrapeYoutubeTrendingData():
     trending_api_data = get_youtube_trending_api()
     print('Data--->', trending_api_data)
 
-with DAG(dag_id="hello_world_dag_adi",
+with DAG(dag_id="yt_trending_scraper_dag",
     start_date=datetime(2021,1,1),
-    schedule_interval="*/1 * * * *",
+    schedule_interval="*/5 * * * *",
     catchup=False) as dag:
     task1 = PythonOperator(
-        task_id="hello_world",
-        python_callable=helloWorld)
+        task_id="yt_trending_scraper_dag",
+        python_callable=scrapeYoutubeTrendingData)
